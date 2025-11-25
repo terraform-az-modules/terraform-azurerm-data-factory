7498312054301f95d8f0a49861bda0be4fd85dba
CloudDrove CI <84795582+clouddrove-ci@users.noreply.github.com>
2025-11-25 21:22:41 +0100

    fix: add description to output id

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,8 @@
 ## Outputs
 ##-----------------------------------------------------------------------------
 output "id" {
-  value = try(azurerm_data_factory.main[0].id, null)
+  value       = try(azurerm_data_factory.main[0].id, null)
+  description = "The ID of the Azure Data Factory."
 }
 
 output "identity" {
